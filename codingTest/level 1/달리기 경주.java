5edda58b207c3c08bbcb51e3447b1e60fb6544e7
heojaehong <56555147+Hjaehong@users.noreply.github.com>
2023-10-30 17:58:54 +0900

    hashmap 이용

--- codingTest/level 1/달리기 경주.java
+++ codingTest/level 1/달리기 경주.java
@@ -17,3 +17,32 @@ class Solution {
     return players;
     }
 }
+
+// 시간 초과 해결하기
+
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> playerIndexMap = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            playerIndexMap.put(players[i], i);
+        }
+        
+        String temp;
+        for (String calling : callings) {
+						// hashMap에서 calling값과 일치하는 index 추출
+            Integer index = playerIndexMap.get(calling);
+            if (index != null && index > 0) {
+                temp = players[index];
+                players[index] = players[index-1];
+                players[index-1] = temp;
+                
+                playerIndexMap.put(players[index], index);
+                playerIndexMap.put(players[index-1], index-1);
+                
+            }
+        }
+        return players;
+    }
+}
